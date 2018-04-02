06a2cb87b49f09b009dfda888604c6a0f3091420
Jim Hayes <jhayes@sdsc.edu>
2018-04-02 15:41:06 -0700

    Build local, older version of boost source that supports eman2 usage.
    Resolves APS-667.

--- src/eman2/version.mk
+++ src/eman2/version.mk
@@ -12,7 +12,7 @@ endif
 
 NAME           = sdsc-eman2
 VERSION        = 2.1
-RELEASE        = 7
+RELEASE        = 8
 PKGROOT        = /opt/eman2
 
 SRC_SUBDIR     = eman2
@@ -23,13 +23,19 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
+BOOST_NAME     = boost
+BOOST_SUFFIX   = tar.gz
+BOOST_VERSION  = 1.55.0
+BOOST_PKG      = $(BOOST_NAME)-$(BOOST_VERSION).$(BOOST_SUFFIX)
+BOOST_DIR      = $(BOOST_PKG:%.$(BOOST_SUFFIX)=%)
+
 FTGL_NAME    = ftgl
 FTGL_SUFFIX  = tar.gz
 FTGL_VERSION = 2.1.3-rc5
 FTGL_PKG     = $(FTGL_NAME)-$(FTGL_VERSION).$(FTGL_SUFFIX)
-FTGL_DIR     = $(FTGL_PKG:%.$(FTGL_SUFFIX)=%)
+FTGL_DIR     = $(subst -rc,~rc,$(FTGL_PKG:%.$(FTGL_SUFFIX)=%))
 
 ZIP_PKGS       = $(SOURCE_PKG)
-TAR_GZ_PKGS    = $(FTGL_PKG)
+TAR_GZ_PKGS    = $(BOOST_PKG) $(FTGL_PKG)
 
 RPM.EXTRAS     = AutoReq:No
