7e8a6c18939d004d671acda61f00e8c3286ca07c
Jerry Greenberg <jpg@sdsc.edu>
2019-01-04 18:39:54 -0800

    APS-872 relion 2.1

--- src/relion/version.mk
+++ src/relion/version.mk
@@ -9,21 +9,21 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-relion
-VERSION        = 1.4
-RELEASE        = 3
+VERSION        = 2.1
+RELEASE        = 0
 PKGROOT        = /opt/relion
 
 SRC_SUBDIR     = relion
 
 SOURCE_NAME    = relion
-SOURCE_SUFFIX  = tar.bz2
+SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 VFLTK          = fltk-1.3.0
 
-TAR_BZ2_PKGS   = $(SOURCE_PKG)
+TAR_GZ_PKGS   = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
 RPM.PREFIX     = $(PKGROOT)
